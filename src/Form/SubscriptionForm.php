03fd86ac247d4807b4f4868b10edac166dad3a97
vinay-osseed <vinay@osseed.com>
2024-02-09 15:37:03 +0530

    feat:  remove empty function

--- src/Form/SubscriptionForm.php
+++ src/Form/SubscriptionForm.php
@@ -110,11 +110,6 @@ class SubscriptionForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {}
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Get `user_group` value.
     $user_group = $this->config->get('user_group');
